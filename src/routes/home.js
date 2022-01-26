cb3046e29b741039c47ed53d9ecdcbe4d9f8b0b2
Joel-Adving <joel.adving@gmail.com>
2022-01-26 22:26:31 +0100

    major style changes

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
+import campgrounds from '../controllers/campgrounds.js'
+import { catchAsync } from '../middleware.js'
+
+const controller = campgrounds()
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('index')
-})
+router.route('/').get(catchAsync(controller.index))
 
 export default router
